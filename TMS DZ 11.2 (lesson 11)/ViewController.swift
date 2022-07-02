e549e9fc917664c61c0c0356ace3e0a9760517d5
Egor Kartshev <egorkarta982@gmail.com>
2022-07-03 01:17:50 +0300

    right button done

--- TMS DZ 11.2 (lesson 11)/ViewController.swift
+++ TMS DZ 11.2 (lesson 11)/ViewController.swift
@@ -3,36 +3,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-//MARK: - IBOutlets
+    //MARK: - IBOutlets
     @IBOutlet var firstImageView: UIImageView!
     
     
-//MARK: - let / var
+    //MARK: - let / var
     
     var secondImageView = UIImageView()
     var arrayImage = ["basket","Foto", "photo3", "photo4"]
-    var countOfArray = 1
+    var countOfArray = 0
+    var startPosition = true
     
-//MARK: - life sycle Func
+    //MARK: - life sycle Func
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.secondImageView.frame = CGRect(x: self.view.frame.width, y: self.firstImageView.frame.origin.y, width: self.firstImageView.frame.width, height: self.firstImageView.frame.height)
         self.view.addSubview(secondImageView)
-        
     }
-
-//MARK: - IBAction
+    
+    //MARK: - IBAction
     
     @IBAction func rightButtonPressed(_ sender: UIButton) {
-        let image = UIImage(named: arrayImage[countOfArray])
-        self.secondImageView.image = image
-        UIView.animate(withDuration: 0.3) {
-            self.secondImageView.frame.origin.x = 0
+        
+        if countOfArray == arrayImage.count - 1 {
+            countOfArray = 0
+        } else {
+            countOfArray += 1
         }
+        
+        if startPosition {
+            
+            
+            let image = UIImage(named: arrayImage[countOfArray])
+            self.secondImageView.image = image
+            UIView.animate(withDuration: 0.3) {
+                self.secondImageView.frame.origin.x = 0
+            } completion: { _ in
+                self.firstImageView.frame.origin.x = self.view.frame.width
+            }
+            print(" first \(self.firstImageView.frame), second \(self.secondImageView.frame)")
+            self.startPosition = !self.startPosition
+            
+        }
+        else {
+            
+            print(" второе нажатие \n first \(self.firstImageView.frame), second \(self.secondImageView.frame)")
+            let image = UIImage(named: arrayImage[countOfArray])
+            self.firstImageView.image = image
+            UIView.animate(withDuration: 0.3) {
+                self.firstImageView.frame.origin.x = 0
+            } completion: { _ in
+                self.secondImageView.frame.origin.x = self.view.frame.width
+            }
+            
+//            print(" first \(self.firstImageView.frame), second \(self.secondImageView.frame)")
+            self.startPosition = !self.startPosition
+        }
+        
+        
     }
-    
-  
-    
+    @IBAction func leftButtonPresed(_ sender: UIButton) {
+        print(" first \(self.firstImageView.frame), second \(self.secondImageView.frame)")
+    }
+   
 }
-
