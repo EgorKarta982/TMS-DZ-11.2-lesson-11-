635c374e598e05a58601caf87199055ea68bb6d5
Egor Kartshev <egorkarta982@gmail.com>
2022-07-02 17:09:13 +0300

    photo album

--- TMS DZ 11.2 (lesson 11)/ViewController.swift
+++ TMS DZ 11.2 (lesson 11)/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  TMS DZ 11.2 (lesson 11)
-//
-//  Created by Егор on 01.07.2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+//MARK: - IBOutlets
+    @IBOutlet var firstImageView: UIImageView!
+    
+    
+//MARK: - let / var
+    
+    var secondImageView = UIImageView()
+    var arrayImage = ["basket","Foto", "photo3", "photo4"]
+    var countOfArray = 1
+    
+//MARK: - life sycle Func
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.secondImageView.frame = CGRect(x: self.view.frame.width, y: self.firstImageView.frame.origin.y, width: self.firstImageView.frame.width, height: self.firstImageView.frame.height)
+        self.view.addSubview(secondImageView)
+        
     }
 
-
+//MARK: - IBAction
+    
+    @IBAction func rightButtonPressed(_ sender: UIButton) {
+        let image = UIImage(named: arrayImage[countOfArray])
+        self.secondImageView.image = image
+        UIView.animate(withDuration: 0.3) {
+            self.secondImageView.frame.origin.x = 0
+        }
+    }
+    
+  
+    
 }
 
